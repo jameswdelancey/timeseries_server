c126679ebb442cfffd189359547b6f085a2768a2
James Delancey <jamesdelanceyjr@gmail.com>
2022-03-21 19:52:52 -0700

    same as last

--- timeseries_server/timeseries_client.py
+++ timeseries_server/timeseries_client.py
@@ -1,10 +1,19 @@
 import json
 import urllib.parse
 import urllib.request
+import os
+
+COLLECTION_SERVER_PROTOCOL = os.environ.get("COLLECTION_SERVER_PROTOCOL", "http")
+COLLECTION_SERVER_ADDRESS = os.environ.get("COLLECTION_SERVER_ADDRESS", "localhost")
+COLLECTION_SERVER_PORT = int(os.environ.get("COLLECTION_SERVER_PORT", 8080))
+url = "%s://%s:%d/timeseries" % (
+    COLLECTION_SERVER_PROTOCOL,
+    COLLECTION_SERVER_ADDRESS,
+    COLLECTION_SERVER_PORT,
+)
 
 
 def send_timeseries(times, entities, keys, values):
-    url = "http://localhost:8080/timeseries"
     values = [
         {
             "time": a,
